51c484c72ac964a7a4b16644bb39be57d71be6d0
Scott Reilly <scott@coffee2code.com>
2017-02-13 20:34:36 -0700

    Note compatibility through WP 4.7+

--- parentless-categories.php
+++ parentless-categories.php
@@ -10,7 +10,7 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * Description: List a post's categories that don't have a parent category also directly assigned to the post (basically lists all leaf nodes).
  *
- * Compatible with WordPress 3.6 through 4.4+.
+ * Compatible with WordPress 3.6 through 4.7+.
  *
  * =>> Read the accompanying readme.txt file for instructions and documentation.
  * =>> Also, visit the plugin's homepage for additional information and updates.
